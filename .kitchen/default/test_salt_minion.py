1987c64de01cddfdeca3ffaae971053c4222ad7a
Paolo Smiraglia <paolo.smiraglia@gmail.com>
2017-06-16 15:33:18 +0200

    Fixed py warnings

--- .kitchen/default/test_salt_minion.py
+++ .kitchen/default/test_salt_minion.py
@@ -1,5 +1,3 @@
-import testinfra
-
-def test_salt_minion_is_installed(Package):
-    salt_minion = Package('salt-minion')
-    assert salt_minion.is_installed
+def test_salt_minion_is_installed(host):
+    pkg = host.package('salt-minion')
+    assert pkg.is_installed
